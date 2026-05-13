c250e91c8d07bb0db47e24ee049a5aac69aad8a6
Zefiryn <zefiryn@jewula.net>
2026-05-13 23:23:04 +0200

    Basic react app structure

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div className="flex flex-col flex-1 items-center justify-center font-sans bg-purple-200">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-purple-200 sm:items-start">
+          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          </div>
+        </main>
+      </div>
+  );
+}
